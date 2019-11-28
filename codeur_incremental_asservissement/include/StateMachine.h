7b25ccc931e8ba6938b9eb212d87fd62aac9ae65
Drodym <hugo.perrin@etu.emse.fr>
2019-11-28 02:23:46 +0100

    Ajout de la classe StateMachine

--- codeur_incremental_asservissement/include/StateMachine.h
+++ codeur_incremental_asservissement/include/StateMachine.h
@@ -1,21 +1,26 @@
 #pragma once
 
 #include "mbed.h"
+#include "Vector2.h"
 
 enum State {
     NONE,
+    IDLE,
     LIGNEDROITE,
-    TOURNER
+    VIRAGE
 };
 
 class StateMachine
 {
     private:
         State _state;
+        double _distance;
+        double _angle;
     public:
         StateMachine();
+        void _update_machine();
         void _state_logic();
         State _get_transition();
-        void _enter_state();
-        void _exit_state();
+        void _enter_state(State new_state, State old_state);
+        void _exit_state(State new_state, State old_state);
 };
--- codeur_incremental_asservissement/src/StateMachine.cpp
+++ codeur_incremental_asservissement/src/StateMachine.cpp
@@ -2,5 +2,79 @@
 
 StateMachine::StateMachine()
 {
-    
+    _state = NONE;
+}
+
+void StateMachine::_update_machine()
+{
+    State transition = _get_transition();
+    if(transition != NONE)
+    {
+        _exit_state(transition, _state);
+        _enter_state(transition, _state);
+        _state = transition;
+    }
+    _state_logic();
+}
+
+void StateMachine::_state_logic()
+{
+    if(_state == LIGNEDROITE)
+    {
+        
+    }
+}
+
+State StateMachine::_get_transition()
+{
+    switch(_state)
+    {
+        case IDLE:
+
+            break;
+        case LIGNEDROITE:
+
+            break;
+        case VIRAGE:
+            
+            break;
+        default:
+            break;
+    }
+}
+
+void StateMachine::_enter_state(State new_state, State old_state)
+{
+    switch(new_state)
+    {
+        case IDLE:
+
+            break;
+        case LIGNEDROITE:
+
+            break;
+        case VIRAGE:
+            
+            break;
+        default:
+            break;
+    }
+}
+
+void StateMachine::_exit_state(State new_state, State old_state)
+{
+    switch(old_state)
+    {
+        case IDLE:
+
+            break;
+        case LIGNEDROITE:
+
+            break;
+        case VIRAGE:
+            
+            break;
+        default:
+            break;
+    }
 }
